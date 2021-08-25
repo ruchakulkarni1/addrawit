0aff733a6aeebf7caf760773aa8f05affa127c8e
ruchakulkarni1 <rucha2@pdx.edu>
2021-08-25 15:52:01 -0700

    about us page

---
+++ addrawit/src/App.js
@@ -0,0 +1,44 @@
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+import About from "./Tabs/about"
+
+function App() {
+  return (
+    <Router>
+      <div>
+      <nav class="navbar navbar-expand-lg navbar-dark ">
+      <a class="navbar-brand inactiveLink"><h1 className="links">AdDrawIt</h1></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsingNavbarMd">
+      <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="navbar-collapse collapse" id="collapsingNavbarMd">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+              <Link to="/"><h4 className="links">Home</h4></Link>
+          </li>
+          <li class="nav-item">
+              <Link to="/shop"><h4 className="links">Shop</h4></Link>
+          </li>
+          <li class="nav-item">
+              <Link to="/about"><h4 className="links">About</h4></Link>
+          </li>
+          <li class="nav-item">
+              <Link to="/contact"><h4 className="links">Contact</h4></Link>
+          </li>
+      </ul>
+  </div>
+</nav>
+<Switch>
+    <Route path="/about">
+      <div class="aboutMainDiv">
+          <About></About>
+      </div>
+    </Route>
+</Switch>
+</div>
+</Router>
+  );
+}
+
+export default App;
---
+++ addrawit/src/Tabs/about.js
@@ -0,0 +1,26 @@
+import React from "react";
+//import 'bootstrap/dist/css/bootstrap.min.css';
+import './about.css'
+
+
+function about(){
+
+    return(
+        <div className="aboutPanel">
+                    <h3 class="title">ABOUT PRODUCT</h3>
+                    <p class="data">It is a digital multicolour laser printout in a Poly-Fiber-Wood border frame.<br/>
+                    <br/>One of its kind state-of-the-art paintings and specially drawn by expert to give a splendid look to your premises.
+                    <br/>This is a homemade business and we need your support to grow.
+                    <br/><br/>This will be the nice gift you can give.
+                    <br/><br/><br/>Order your frame now...!!</p>
+        </div>
+    )
+}
+
+export default about;
+
+/**
+ * <div className="titlediv">
+                <h3 class="title">A B O U T   P R O D U C T</h3>
+            </div>
+ */
